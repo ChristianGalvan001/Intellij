dbb47c0d67250382c910b045b30bb6b1b27383b7
ChristianGalvan001 <Themanon81@gmail.com>
2019-04-29 12:43:20 -0600

    added new method and thread

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -9,23 +9,34 @@ import java.util.Date;
 
 public class ServerMain {
     //telnet localhost 8818
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int port = 8818;
-        try{
-        ServerSocket serverSocket = new ServerSocket(port);
-        while(true) {
-            System.out.println("About to accept client connection...");
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Accepted connetion from"+ clientSocket);
-            handleClientSocket(clientSocket);
-        }
-        } catch ( IOException e){
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            while (true) {
+                System.out.println("About to accept client connection...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connetion from" + clientSocket);
+                Thread t = new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            handleClientSocket(clientSocket);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                };
+                t.start();
+            }
+        } catch (IOException e) {
             e.printStackTrace();
-        } catch ( InterruptedException e){
-            e.printStackTrace();
-        }
 
 
+        }
     }
 
     private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
