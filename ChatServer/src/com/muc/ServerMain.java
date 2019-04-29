6f2d602c7f1f2ea07c9f615ea1d433cab8b0dee4
ChristianGalvan001 <Themanon81@gmail.com>
2019-04-29 12:26:01 -0600

    first commit

---
+++ ChatServer/src/com/muc/ServerMain.java
@@ -0,0 +1,36 @@
+package com.muc;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+
+public class ServerMain {
+    //telnet localhost 8818
+    public static void main(String[] args){
+        int port = 8818;
+        try{
+        ServerSocket serverSocket = new ServerSocket(port);
+        while(true) {
+            System.out.println("About to accept client connection...");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Accepted connetion from"+ clientSocket);
+            OutputStream outputStream = clientSocket.getOutputStream();
+            outputStream.write("Christian Galvan is here.\n".getBytes());
+            for(int i=0; i < 10; i++){
+                outputStream.write(("Time now is " + new Date() + "\n").getBytes());
+                Thread.sleep(1000);
+            }
+            clientSocket.close();
+        }
+        } catch ( IOException e){
+            e.printStackTrace();
+        } catch ( InterruptedException e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
